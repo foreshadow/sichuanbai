9cd4d5f1dd835e94960d2f38492a7fd8ad63228e
Infinity <573679956@qq.com>
2016-12-03 17:03:00 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+if (!isset($_GET['q'])) {
+    $id = 1;
+} else 
+$id = $_GET['q'];
+$sqlite = new SQLite3('data/data.sqlite');
+$statement = $sqlite->prepare('select * from clue where id = :id');
+$statement->bindValue(':id', $id, SQLITE3_INTEGER);
+$result = $statement->execute();
+$row = $result->fetchArray(SQLITE3_ASSOC);
+$sqlite->close();
+?>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $row['title']; ?> - 死穿白</title>
+  <!-- Bootstrap core CSS -->
+  <link href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+      <a href="#" class="navbar-text pull-right" style="margin-right: 15px; text-decoration: none;">返回</a>
+      <a class="navbar-brand" href="#">死穿白</a>
+    </div>
+  </nav>
+  <div class="container" style="margin-top: 70px;">
+    <?php if ($id > 1) : ?>
+    <a class="btn btn-default" href="?q=<?php echo $id - 1; ?>">Prev</a>
+    <?php endif; if ($id < 56) : ?>
+    <a class="btn btn-default" href="?q=<?php echo $id + 1; ?>">Next</a>
+    <?php endif; ?>
+    <div class="clue"> 
+      <h3><span class="glyphicon glyphicon-search"></span> <?php echo $row['title']; ?></h3>
+      <p><?php echo str_replace("\n", '</p><p>', $row['content']); ?></p>
+      <?php if ($row['img']) : ?>
+      <img src="img/clue/<?php echo $row['img']; ?>">
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+
+</html>
